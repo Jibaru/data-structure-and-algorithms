830d93f814230fcae46dc5a096ce9577ed4a7429
Ignacio <ignacioruedaboada@hotmail.com>
2020-07-17 15:10:20 -0500

    mas ejercicios

--- 1.POO/EJERCICIOS/11.cpp
+++ 1.POO/EJERCICIOS/11.cpp
@@ -9,3 +9,68 @@ c)	Imprimir todos los datos de MiMaestra.
 d)	Imprimir, si MiMaestra está casada, el nombre de su cónyuge.
 
 */
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Persona {
+private:
+    char _nombre[45];
+    char _direccion[64];
+    char _telefono[10];
+    bool _casado;
+    char _nombreConyuge[45];
+
+public:
+    Persona(char[], char[], char[], bool, char[]);
+    void cambiarDireccion(char []);
+    void cambiarTelefono(char []);
+    void imprimir();
+    void imprimirNombreConyuge();
+};
+
+Persona::Persona(char nombre[45], char direccion[64], char telefono[10], bool casado, char nombreConyuge[45])
+{
+    strcpy(_nombre, nombre);
+    strcpy(_direccion, direccion);
+    strcpy(_telefono, telefono);
+    _casado = casado;
+    strcpy(_nombreConyuge, nombreConyuge);
+}
+
+void Persona::cambiarDireccion(char direccion[45])
+{
+    strcpy(_direccion, direccion);
+}
+
+void Persona::cambiarTelefono(char telefono[])
+{
+    strcpy(_telefono, telefono);
+}
+
+void Persona::imprimir()
+{
+    cout << "Nombre: " << _nombre << endl;
+    cout << "Direccion: " << _direccion << endl;
+    cout << "Telefono: " << _telefono << endl;
+    cout << "Casado: " << (_casado ? "SI" : "NO") << endl;
+    imprimirNombreConyuge();
+}
+
+void Persona::imprimirNombreConyuge()
+{
+    if(_casado) cout << "Nombre conyuge: " << _nombreConyuge << endl;
+}
+
+int main(){
+
+    Persona MiMaestra("Ana Banana", "Calle San Fernando 123", "985623221", true, "Pedro Carbonel");
+
+    MiMaestra.cambiarDireccion("Calle San Jose 453");
+    MiMaestra.cambiarTelefono("966553269");
+    MiMaestra.imprimir();
+    MiMaestra.imprimirNombreConyuge();
+
+    return 0;
+}
