8a2d733a9b31084762893cea525770f5a795f32b
Ignacio <ignacioruedaboada@hotmail.com>
2020-07-19 18:42:07 -0500

    Parte de ejercicio 4 capitulo 2

--- 2.HERENCIA_AMISTAD/EJERCICIOS/4.cpp
+++ 2.HERENCIA_AMISTAD/EJERCICIOS/4.cpp
@@ -1,10 +1,37 @@
 /*
-4.	Definir la clase FiguraGeometrica que contenga los atributos que caracterizan a toda figura geométrica y los métodos necesarios para manejarlos. Además, defina las clases derivadas Cuadrado y Triangulo. Escriba un programa en C++ que haga uso de estas clases para calcular el total de metros cuadrados de tela necesaria para fabricar N almohadones con forma cuadrada y M almohadones con forma de triángulo equilátero. Los datos que se ingresarán al programa son: 
+4. Definir la clase FiguraGeometrica que contenga los atributos que caracterizan a toda figura 
+geométrica y los métodos necesarios para manejarlos. Además, defina las clases derivadas Cuadrado y
+Triangulo. Escriba un programa en C++ que haga uso de estas clases para calcular el total de metros 
+cuadrados de tela necesaria para fabricar N almohadones con forma cuadrada y M almohadones con forma 
+de triángulo equilátero. Los datos que se ingresarán al programa son: 
 Datos:
-• N: total de almohadones con forma cuadrada.
-• LadoC: tamaño, en centímetros, de cada uno de los lados del almoha¬dón cuadrado.
-•	M: total de almohadones con forma triangular.
-•	LadoT: tamaño, en centímetros, de cada uno de los lados del almoha¬dón triangular.
+•N: total de almohadones con forma cuadrada.
+•LadoC: tamaño, en centímetros, de cada uno de los lados del almoha¬dón cuadrado.
+•M: total de almohadones con forma triangular.
+•LadoT: tamaño, en centímetros, de cada uno de los lados del almoha¬dón triangular.
 Resultado esperado: Total de metros cuadrados requeridos para la fabricación de los N + M almohadones.
 
 */
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class FiguraGeometrica {
+protected:
+    int numLados;
+    char* nombre;
+public:
+    FiguraGeometrica(int, char*);
+    void cambiarNumLados(int);
+    void cambiarNombre(char*);
+    int obtenerNumLados();
+    char* obtenerNombre();
+    void imprimirDatos();
+};
+
+FiguraGeometrica::FiguraGeometrica(int numLad, char* nom)
+{
+    numLados = numLad;
+    nombre = new char[strlen(nom) + 1];
+    strcpy(nombre, nom);
+}
