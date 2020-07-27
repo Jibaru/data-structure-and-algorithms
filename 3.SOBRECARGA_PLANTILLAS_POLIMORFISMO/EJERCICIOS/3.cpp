5fbdbc94a647f649c826ce8b7ed646b9872aa091
jibaru <xxnacho1997xx@gmail.com>
2020-07-27 16:44:22 -0500

    Ejercicios 3 y 4 seccion 3

--- 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/3.cpp
+++ 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/3.cpp
@@ -14,3 +14,59 @@ void Imprime()
 friend istream... 
 friend ostream...
 */
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Fruta {
+private:
+	char NombreFruta[64];
+	char EstacionCosecha[64];
+public:
+	Fruta();
+	Fruta(char*, char*);
+	int operator==(Fruta&);
+	void Imprime();
+	friend istream& operator>>(istream&, Fruta&);
+	friend ostream& operator<<(ostream&, Fruta&);
+};
+
+Fruta::Fruta() {}
+
+Fruta::Fruta(char* nombre, char* estacion)
+{	
+	strcpy(NombreFruta, nombre);
+	strcpy(EstacionCosecha, estacion);
+}
+
+int Fruta::operator==(Fruta& otro)
+{
+	if(strcmp(NombreFruta, otro.NombreFruta) == 0 && 
+	   strcmp(EstacionCosecha, otro.EstacionCosecha) == 0){
+		return 1;   
+	}
+	return 0;
+}
+
+void Fruta::Imprime() {
+	cout << "Datos de la fruta" << endl;
+	cout << "Nombre: " << NombreFruta << endl;
+	cout << "Estacion de cosecha: " << EstacionCosecha << endl;
+}
+
+istream& operator>>(istream& lee, Fruta& obj)
+{
+	cout << "\nIngrese nombre fruta: ";
+	lee.getline(obj.NombreFruta, 64);
+	cout << "\nEstacion de cosecha: ";
+	lee.getline(obj.EstacionCosecha, 64);
+	return lee;
+}
+
+ostream& operator<<(ostream& escribe, Fruta& obj)
+{
+	escribe << "Datos de la Fruta: " << endl;
+	escribe << "Nombre: " << obj.NombreFruta << endl;
+	escribe << "Estacion de cosecha: " << obj.EstacionCosecha << endl;
+	return escribe;
+}
--- 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/4.cpp
+++ 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/4.cpp
@@ -1,5 +1,26 @@
 /*
-4.	Retome la clase definida en el ejercicio anterior. Escriba un programa en C++ que:
-a)	Cree dos objetos tipo Fruta, asignándole valores a sus atributos por medio del operador >> sobrecargado.
+4.	Retome la clase definida en el ejercicio anterior. Escriba un programa en 
+C++ que:
+a)	Cree dos objetos tipo Fruta, asignándole valores a sus atributos por medio
+ del operador >> sobrecargado.
 b)	Compare los objetos e indique si son iguales. Imprima un mensaje adecuado.
 */
+
+#include "3.cpp"
+
+int main() {
+	
+	Fruta ObjFruta1, ObjFruta2;
+	
+	cin >> ObjFruta1 >> ObjFruta2;
+	
+	if(ObjFruta1 == ObjFruta2) {
+		cout << "\nFrutas iguales\n" << endl;
+	} else {
+		cout << "\nFrutas diferentes\n" << endl;
+	}
+	
+	cout << ObjFruta1 << endl << ObjFruta2 << endl;
+	
+	return 0;
+}
