e7ece2ec2874a78e36f01ec071d80cdc578c1e66
Ignacio <ignacioruedaboada@hotmail.com>
2020-07-26 20:07:42 -0500

    Ejercicios

---
+++ 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/1.cpp
@@ -0,0 +1,16 @@
+/*1. Defina la clase CadenaCar según las especificaciones que se muestran a 
+continuación. Incluya la sobrecarga de los siguientes operadores: ==, !=, +, < y >, 
+de tal manera que dos objetos tipo CadenaCar se puedan comparar (=, !=, <, >) o 
+unir (+) usando los operadores indicados.
+CadenaCar
+Tam: int
+Cadena: char[]
+Constructor(es) 
+int operator==(CadenaCar) 
+int operator!=(CadenaCar) 
+int operator<(CadenaCar) 
+int operator>(CadenaCar) 
+CadenaCar operator+(CadenaCar) 
+void Imprime()*/
+
+
