de5c3149dde2c7fa1212366f01f4e4476854c6f3
jibaru <xxnacho1997xx@gmail.com>
2020-07-27 16:21:06 -0500

    Ejercicios 1 y 2 seccion 3

--- 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/1.cpp
+++ 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/1.cpp
@@ -12,5 +12,64 @@ int operator<(CadenaCar)
 int operator>(CadenaCar) 
 CadenaCar operator+(CadenaCar) 
 void Imprime()*/
+#include <iostream>
+#include <string.h>
+using namespace std;
 
+class CadenaCar {
+private:
+	int Tam;
+	char* Cadena;
+public:
+	CadenaCar();
+	CadenaCar(char*);
+	int operator==(CadenaCar&);
+	int operator!=(CadenaCar&);
+	int operator<(CadenaCar&);
+	int operator>(CadenaCar&);
+	CadenaCar operator+(CadenaCar&);
+	void Imprime();
+};
 
+CadenaCar::CadenaCar(){}
+
+CadenaCar::CadenaCar(char* cadena)
+{
+	Tam = strlen(cadena);
+	Cadena = new char[Tam + 1];
+	strcpy(Cadena, cadena);
+}
+
+int CadenaCar::operator==(CadenaCar& otro)
+{
+	return strcmp(Cadena, otro.Cadena);
+}
+
+int CadenaCar::operator!=(CadenaCar& otro)
+{
+	return strcmp(Cadena, otro.Cadena);
+}
+
+int CadenaCar::operator<(CadenaCar& otro)
+{
+	return strcmp(Cadena, otro.Cadena);
+}
+
+int CadenaCar::operator>(CadenaCar& otro)
+{
+	return strcmp(Cadena, otro.Cadena);
+}
+
+CadenaCar CadenaCar::operator+(CadenaCar& otro)
+{
+	char* cad = new char[strlen(Cadena) + strlen(otro.Cadena) + 1];
+	strcpy(cad, Cadena);
+	strcat(cad, otro.Cadena);
+	return CadenaCar(cad);
+}
+
+void CadenaCar::Imprime()
+{
+	cout << "Cadena: " << Cadena << endl;
+	cout << "Tamanio: " << Tam << endl;
+}
--- 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/2.cpp
+++ 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/2.cpp
@@ -2,7 +2,32 @@
 2. Retome la clase definida en el ejercicio anterior. Escriba un programa en C++ 
 que:
 a)	Declare dos objetos tipo CadenaCar.
-b)	Le asigne una cadena de caracteres a cada uno de los objetos. La asignación puede ser a través de una lectura o por medio del método constructor.
-c)	Compare los objetos e imprima un mensaje adecuado si los mismos son iguales. Si no lo fueran, el mensaje, además de indicar este caso, debe decir cuál de las cadenas es menor.
-d)	Enlace dos objetos tipo CadenaCar formando un tercer objeto del mismo tipo. Imprima el objeto resultante.
+b)	Le asigne una cadena de caracteres a cada uno de los objetos. La asignación puede 
+ser a través de una lectura o por medio del método constructor.
+c)	Compare los objetos e imprima un mensaje adecuado si los mismos son iguales. Si 
+no lo fueran, el mensaje, además de indicar este caso, debe decir cuál de las cadenas
+ es menor.
+d)	Enlace dos objetos tipo CadenaCar formando un tercer objeto del mismo tipo. 
+Imprima el objeto resultante.
 */
+
+#include "1.cpp"
+
+int main() {
+	
+	CadenaCar ObjCadena1((char*)"Cadena1"), ObjCadena2((char*)"Cadena2");
+	
+	if(ObjCadena1 == ObjCadena2) {
+		cout << "Las cadenas son iguales" << endl;
+	} else if(ObjCadena1 < ObjCadena2) {
+		cout << "La cadena 1 es menor a la cadena 2" << endl;
+	} else if(ObjCadena1 > ObjCadena2) {
+		cout << "La cadena 1 es mayor a la cadena 2" << endl;
+	}
+	
+	CadenaCar ObjCadena3 = ObjCadena1 + ObjCadena2;
+	
+	ObjCadena3.Imprime();
+	
+	return 0;
+}
