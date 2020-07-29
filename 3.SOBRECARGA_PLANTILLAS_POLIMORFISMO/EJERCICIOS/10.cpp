91589cc72ab94e3d3c22d09154c8d68fd08ba2c0
jibaru <xxnacho1997xx@gmail.com>
2020-07-28 21:43:13 -0500

    Ejercicios 9 y 10 seccion 3

--- 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/10.cpp
+++ 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/10.cpp
@@ -1,6 +1,34 @@
 /*
-10.	Retome el problema 8, pero ahora utilice una clase Materia para instanciar T. Puede usar la 
-del problema 5 o definir su propia clase. ¿Debe modificar la plantilla de la clase Prof esor?, o 
-¿debe modificar la otra clase? Si usó la plantilla del problema 5, ¿cuántos valores para T debe 
-dar al crear un objeto de tipo Profesor?
+10.	Retome el problema 8, pero ahora utilice una clase Materia para instanciar
+ T. Puede usar la del problema 5 o definir su propia clase. 
+¿Debe modificar la plantilla de la clase Profesor?,
+No, no es necesario 
+¿debe modificar la otra clase? Si usó la plantilla del problema 5, 
+Si, es necesario sobrecargar el operador <<
+¿cuántos valores para T debe dar al crear un objeto de tipo Profesor?
+2 valores, Materia y float
 */
+
+#include "5.cpp"
+#include "7.cpp"
+
+int main() {
+	
+	Materia<float> materia((char*)"Historia", 7, 17.5);
+	
+	Profesor<Materia<float>> ProfeJuan((char*)"Profesor Juan", (char*)"Lima", 2020, materia);
+	
+	ProfeJuan.Imprime();
+	
+	ProfeJuan.CambiaDepto((char*)"La Libertad");
+	
+	ProfeJuan.Imprime();
+	
+	Materia<float> nuevaMateria((char*)"Calculo", 9, 19);
+	
+	ProfeJuan.CambiaMat(nuevaMateria);
+	
+	ProfeJuan.Imprime();
+	
+	return 0;
+}
--- 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/9.cpp
+++ 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/9.cpp
@@ -1,4 +1,26 @@
 /*
-9.	Retome el problema 8, pero ahora utilice una cadena de caracteres para instanciar T. 
-¿Debe modificar la plantilla de la clase Profesor?, o ¿debe definir alguna otra clase?
+9.	Retome el problema 8, pero ahora utilice una cadena de caracteres para 
+instanciar T. 
+¿Debe modificar la plantilla de la clase Profesor?, o ¿debe definir alguna 
+otra clase?
+Rpta: No es necesario
 */
+
+#include "7.cpp"
+
+int main() {
+	
+	Profesor<char*> ProfeJuan((char*)"Profesor Juan", (char*)"Lima", 2020, (char*)"Historia");
+	
+	ProfeJuan.Imprime();
+	
+	ProfeJuan.CambiaDepto((char*)"La Libertad");
+	
+	ProfeJuan.Imprime();
+	
+	ProfeJuan.CambiaMat((char*)"Calculo");
+	
+	ProfeJuan.Imprime();
+	
+	return 0;
+}
