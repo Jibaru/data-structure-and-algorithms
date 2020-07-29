292542188d1551bdc4cfa73d78e19978d083c96f
jibaru <xxnacho1997xx@gmail.com>
2020-07-28 21:18:00 -0500

    Ejercicios 7 y 8 seccion 3

--- 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/7.cpp
+++ 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/7.cpp
@@ -1,8 +1,10 @@
 /*
-7.	Defina la plantilla de la clase Prof esor según las especificaciones que se dan más adelante. 
-El atributo MateriaACargo es del tipo T, en este caso podría ser un entero (si la materia se representa 
-por medio de una clave), una cadena de caracteres (si la materia se representa por su nombre) u otro 
-objeto (si la materia se representa usando una clase previamente definida).
+7.	Defina la plantilla de la clase Profesor según las especificaciones que se
+dan más adelante. 
+El atributo MateriaACargo es del tipo T, en este caso podría ser un entero 
+(si la materia se representa por medio de una clave), una cadena de caracteres
+(si la materia se representa por su nombre) u otro objeto (si la materia se 
+representa usando una clase previamente definida).
 Profesor
 NombreProfesor: char[]
 Departamento: char[]
@@ -13,3 +15,54 @@ void CambiaDepto(char[])
 void CambiaMat(T)
 void Imprime()
 */
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+template <class T>
+class Profesor {
+private:
+	char NombreProfesor[64];
+	char Departamento[64];
+	int AnioIngreso;
+	T MateriaACargo;
+public:
+	Profesor();
+	Profesor(char*, char*, int, T);
+	void CambiaDepto(char*);
+	void CambiaMat(T);
+	void Imprime();
+};
+
+template <class T>
+Profesor<T>::Profesor() {}
+
+template <class T>
+Profesor<T>::Profesor(char* nombre, char* depto, int anio, T mat)
+{
+	strcpy(NombreProfesor, nombre);
+	strcpy(Departamento, depto);
+	AnioIngreso = anio;
+	MateriaACargo = mat;
+}
+
+template <class T>
+void Profesor<T>::CambiaDepto(char* dpto)
+{
+	strcpy(Departamento, dpto);
+}
+
+template <class T>
+void Profesor<T>::CambiaMat(T mat)
+{
+	MateriaACargo = mat;
+}
+
+template <class T>
+void Profesor<T>::Imprime()
+{
+	cout << "\nNombre: " << NombreProfesor << endl;
+	cout << "Anio ingreso: " << AnioIngreso << endl;
+	cout << "Departamento: " << Departamento << endl;
+	cout << "Materia a cargo: " << MateriaACargo << endl << endl;
+}
--- 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/8.cpp
+++ 3.SOBRECARGA_PLANTILLAS_POLIMORFISMO/EJERCICIOS/8.cpp
@@ -1,8 +1,27 @@
 /*
-8.	Retome la clase definida en el ejercicio anterior. Escriba un programa en C++ que:
+8.	Retome la clase definida en el ejercicio anterior. Escriba un programa en 
+C++ que:
 a)	Cree el objeto ProfeJuan de tipo Profesor, usando int para instanciar T.
 b)	Imprima todos los datos del ProfeJuan.
 c)	Cambie el nombre del departamento al cual está adscrito el ProfeJuan.
 d)	Cambie la materia que tiene a cargo el ProfeJuan.
-
 */
+
+#include "7.cpp"
+
+int main() {
+	
+	Profesor<int> ProfeJuan((char*)"Profesor Juan", (char*)"Lima", 2020, 7);
+	
+	ProfeJuan.Imprime();
+	
+	ProfeJuan.CambiaDepto((char*)"La Libertad");
+	
+	ProfeJuan.Imprime();
+	
+	ProfeJuan.CambiaMat(8);
+	
+	ProfeJuan.Imprime();
+	
+	return 0;
+}
