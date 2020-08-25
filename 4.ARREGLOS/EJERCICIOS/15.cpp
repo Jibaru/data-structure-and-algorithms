4e6cb6656940f855e532a93704afae45e6d25abd
Ignacio <ignacioruedaboada@hotmail.com>
2020-08-24 19:02:41 -0500

    Seccion 4 ejercicio 15

--- 4.ARREGLOS/EJERCICIOS/15.cpp
+++ 4.ARREGLOS/EJERCICIOS/15.cpp
@@ -36,10 +36,13 @@ Impresión
 
 #include <iostream>
 #include <string.h>
+#include <conio.h>
 
 #define MAX 100
 
 using namespace std;
+
+class ArregloAlumnos;
 
 class Alumno {
 private:
@@ -52,9 +55,13 @@ public:
 	Alumno();
 	Alumno(char* nom, char* carr, int nroMatAprob, int nroMatReprob);
 	void lectura();
+	void lecturaPorCarrera(char* carr);
 	float calculaPromedio();
 	void cambiaCarrera(char* carr);
 	void imprimeDatos();
+	bool operator==(Alumno&);
+	bool operator!=(Alumno&);
+	friend class ArregloAlumnos;
 };
 
 Alumno::Alumno(){}
@@ -78,6 +85,7 @@ void Alumno::lectura()
 {
 	char nom[100], carr[100];
 	cout << "Nombre: ";
+	cin.ignore();
 	cin.getline(nom, 100);
 	
 	nombre = new char[strlen(nom) + 1];
@@ -96,9 +104,37 @@ void Alumno::lectura()
 	cout << "Nro Materias Aprobadas: ";
 	cin >> nroMateriasAprob;
 	cout << "Nro Materias Reprobadas: ";
-	cin >> nroMatReprob;
+	cin >> nroMateriasReprob;
 	
-	for(int i = 0; i < nroMatAprob; i++) {
+	for(int i = 0; i < nroMateriasAprob; i++) {
+		cout << "Calif. Materia Aprobada " << i + 1 << ": ";
+		cin >> califMateriasAprob[i];
+	}
+}
+
+void Alumno::lecturaPorCarrera(char* carr)
+{
+	char nom[100];
+	cout << "Nombre: ";
+	cin.ignore();
+	cin.getline(nom, 100);
+	
+	nombre = new char[strlen(nom) + 1];
+	if(nombre != NULL) {
+		strcpy(nombre, nom);
+	}
+	
+	carrera = new char[strlen(carr) + 1];
+	if(nombre != NULL) {
+		strcpy(carrera, carr);
+	}
+	
+	cout << "Nro Materias Aprobadas: ";
+	cin >> nroMateriasAprob;
+	cout << "Nro Materias Reprobadas: ";
+	cin >> nroMateriasReprob;
+	
+	for(int i = 0; i < nroMateriasAprob; i++) {
 		cout << "Calif. Materia Aprobada " << i + 1 << ": ";
 		cin >> califMateriasAprob[i];
 	}
@@ -107,16 +143,15 @@ void Alumno::lectura()
 float Alumno::calculaPromedio()
 {
 	float sum = 0;
-	for(int i = 0; i < nroMatAprob; i++) {
+	for(int i = 0; i < nroMateriasAprob; i++) {
 		sum += califMateriasAprob[i];
 	}
 	
-	return sum / nroMatAprob;
+	return sum / nroMateriasAprob;
 }
 
 void Alumno::cambiaCarrera(char* carr)
 {
-	delete carrera;
 	carrera = new char(strlen(carr) + 1);
 	
 	if(carrera != NULL) {
@@ -131,11 +166,33 @@ void Alumno::imprimeDatos()
 	cout << "Nro. Materias Aprob.: " << nroMateriasAprob << endl;
 	cout << "Nro. Materias Reprob.: " << nroMateriasReprob << endl;
 	
-	for(int i = 0; i < nroMatAprob; i++) {
+	for(int i = 0; i < nroMateriasAprob; i++) {
 		cout << "Calif. Aprob. " << i + 1 << ": " << califMateriasAprob[i] << endl;
 	}
 	
 	cout << "Promedio: " << calculaPromedio() << endl;
+}
+
+bool Alumno::operator==(Alumno& otro)
+{
+	if(nroMateriasAprob != otro.nroMateriasAprob) {
+		return false;
+	}
+	if(nroMateriasReprob != otro.nroMateriasReprob) {
+		return false;
+	}
+	if(strcmp(nombre, otro.nombre) != 0) {
+		return false;
+	}
+	if(strcmp(carrera, otro.carrera) != 0) {
+		return false;
+	}
+	return true;
+}
+
+bool Alumno::operator!=(Alumno& otro)
+{
+	return !(*this == otro);
 }
 
 class ArregloAlumnos {
@@ -149,7 +206,11 @@ public:
 	int insertar(Alumno a);
 	int eliminar(int pos);
 	int buscar(Alumno a);
-	Alumno obtener(int pos);
+	Alumno* obtener(int pos);
+	int tamanio();
+	void mostrarConMasDe25MatAprob();
+	void mostrarPromGrupalIndividual();
+	void darDeBajaConMasMatReprobQueAprob();
 };
 
 ArregloAlumnos::ArregloAlumnos()
@@ -207,21 +268,26 @@ int ArregloAlumnos::eliminar(int pos)
 	return resultado;
 }
 
-Alumno ArregloAlumnos::obtener(int pos)
+Alumno* ArregloAlumnos::obtener(int pos)
 {
-	return datos[pos];
+	return &datos[pos];
+}
+
+int ArregloAlumnos::tamanio()
+{
+	return tam;
 }
 
 void ArregloAlumnos::lectura()
 {
-	Alumno a();
+	Alumno a = Alumno();
 	do {
 		cout << "Ingrese el tamanio: ";
 		cin >> tam;
 	} while(tam > MAX || tam <= 0);
 	
 	for(int i = 0; i < tam; i++) {
-		cout << "Alumno: " << i + 1 << ": ";
+		cout << "Alumno: " << i + 1 << ": " << endl;
 		a.lectura();
 		datos[i] = a;
 	}
@@ -230,9 +296,39 @@ void ArregloAlumnos::lectura()
 void ArregloAlumnos::escritura()
 {
 	for(int i = 0; i < tam; i++) {
-		cout << "Alumno: " << i + 1 << ": ";
-		a.imprimeDatos();
+		cout << "Alumno: " << i + 1 << ": " << endl;
+		datos[i].imprimeDatos();
 		cout << endl;
+	}
+}
+
+void ArregloAlumnos::mostrarConMasDe25MatAprob()
+{
+	for(int i = 0; i < tam; i++) {
+		if(datos[i].nroMateriasAprob > 25) {
+			cout << "\nAlumno " << i + 1 << ":" << endl;
+			datos[i].imprimeDatos();
+		}
+	}
+}
+
+void ArregloAlumnos::mostrarPromGrupalIndividual()
+{
+	float sumTotal = 0;
+	for(int i = 0; i < tam; i++) {
+		cout << "\nAlumno " << i + 1 << ":" << endl;
+		datos[i].imprimeDatos();
+		sumTotal += datos[i].calculaPromedio();
+	}
+	cout << "\nPromedio de grupo: " << (sumTotal / tam) << endl;
+}
+
+void ArregloAlumnos::darDeBajaConMasMatReprobQueAprob()
+{
+	for(int i = 0; i < tam; i++) {
+		if(datos[i].nroMateriasReprob > datos[i].nroMateriasAprob) {
+			eliminar(i);
+		}
 	}
 }
 
@@ -243,6 +339,95 @@ int menuOpciones()
 	do {
 		cout << "1-Ingresar alumnos por carrera" << endl;
 		cout << "2-Promedio de alumnos (por cada uno y total)" << endl;
-		cout << "3-"
-	}
+		cout << "3-Cambiar carrera por posicion" << endl;
+		cout << "4-Alumnos con mas de 25 materias aprobadas" << endl;
+		cout << "5-Dar de baja a alumnos con mat. desap. mayores o iguales a mat. aprob" << endl;
+		cout << "6-Dar de alta a un nuevo alumno" << endl;
+		cout << "7-Ver todos" << endl;
+		cout << "8-Salir" << endl;
+		cout << "Ingrese opcion: ";
+		cin >> opc;
+	} while(opc < 1 || opc > 8);
+	return opc;
+}
+
+int main()
+{
+	ArregloAlumnos alumnos = ArregloAlumnos();
+	Alumno alm;
+
+	int opc, resul, sumTotal, pos, numAlum;
+	char carrera[60];
+	do {
+		opc = menuOpciones();
+		switch (opc)
+		{
+			case 1: {
+				cout << "Cantidad de alumnos: ";
+				cin >> numAlum;
+				cout << "Carrera: ";
+				cin.ignore();
+				cin.getline(carrera, 60);
+				for(pos = 0; pos < numAlum; pos++) {
+					alm = Alumno();
+					alm.lecturaPorCarrera(carrera);
+					alumnos.insertar(alm);
+				}
+				break;
+			}
+			case 2: {
+				alumnos.mostrarPromGrupalIndividual();
+				break;
+			}
+
+			case 3: {
+				cout << "Posicion: ";
+				cin >> pos;
+				if(pos < alumnos.tamanio() && pos >= 0) {
+					cout << "Nueva carrera: ";
+					cin.ignore();
+					cin.getline(carrera, 60);
+					alumnos.obtener(pos)->cambiaCarrera(carrera);
+				} else {
+					cout << "No existe el alumno" << endl;
+				}
+				break;
+			}
+			case 4: {
+				alumnos.mostrarConMasDe25MatAprob();
+				break;
+			}
+
+			case 5: {
+				alumnos.darDeBajaConMasMatReprobQueAprob();
+				break;
+			}
+
+			case 6: {
+				alm = Alumno();
+				alm.lectura();
+				resul = alumnos.insertar(alm);
+				if(resul == 1) {
+					cout << "Alumno registrado" << endl;
+				} else if(resul == 0) {
+					cout << "No se pudo registrar el alumno" << endl;
+				}
+				break;
+			}
+
+			case 7: {
+				alumnos.escritura();
+				break;
+			}
+
+			case 8: {
+				cout << "Programa terminado" << endl;
+				break;
+			}
+		
+		}
+		getch();
+	} while(opc != 8);
+
+	return 0;
 }
