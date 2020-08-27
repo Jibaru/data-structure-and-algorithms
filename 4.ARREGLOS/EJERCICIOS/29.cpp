9286f494786e6ce013a1c161b642bdcd26468e3d
Ignacio <ignacioruedaboada@hotmail.com>
2020-08-27 09:56:12 -0500

    Seccion 4 ejercicio 29

--- 4.ARREGLOS/EJERCICIOS/29.cpp
+++ 4.ARREGLOS/EJERCICIOS/29.cpp
@@ -6,3 +6,121 @@ resulta conveniente guardar sólo la matriz triangular inferior o superior,
 ya que de lo contrario se duplicaría la información. Defina una plantilla 
 para una clase que represente este tipo de matrices.
 */
+#include <iostream>
+
+using namespace std;
+#define MAX 50
+
+/* Definición de la clase MatrizSimetrica. Sus atributos son un arreglo 
+unidimensional en el cual se guardarán los valores de la matriz 
+triangular simetrica y el orden de la misma. */
+template <class T>
+class MatrizSimetrica
+{
+private:
+    T Datos[MAX]; int Dim;
+public:
+    MatrizSimetrica();
+    int RegresaPosic(int, int);
+    int TotalDatos();
+    void Lectura();
+    void ImprimeMatriz();
+    void ImprimeDatos();
+};
+
+/* Método constructor por omisión. */
+template <class T>
+MatrizSimetrica<T>::MatrizSimetrica()
+{}
+
+/* Método que calcula la posición que le corresponde a un elemento de la 
+matriz dentro del arreglo unidimensional en el cual fue guardado. */
+template <class T>
+int MatrizSimetrica<T>::RegresaPosic(int Ren, int Col)
+{
+    return ((Ren * (Ren + 1)) / 2) + Col;
+}
+
+/* Método que calcula el total de elementos guardados en el arreglo
+unidimensional, que son los que estaban de la diagonal principal hacia
+arriba. */
+template <class T>
+int MatrizSimetrica<T>::TotalDatos()
+{
+    return ((Dim * (Dim + 1)) / 2) ;
+}
+
+/* Método que lee del teclado los valores para los atributos de la clase.
+Al usuario sólo se le piden los valores que están en la diagonal arriba de ella. */
+template <class T>
+void MatrizSimetrica<T>::Lectura()
+{
+    int Ren, Col, Indice= 0;
+    do {
+        cout << "\n\nIngrese orden de la matriz simetrica: ";
+        cin >> Dim;
+    } while (Dim > MAX || Dim < 0);
+    
+    for (Ren= 0; Ren < Dim; Ren++)
+    {
+        for (Col= Ren; Col < Dim; Col++) 
+        {
+            cout << "\n\nIngrese el elemento " << Ren + 1 << " - " << Col + 1 << ": ";
+            cin >> Datos[Indice];
+            Indice= Indice + 1;
+        }
+    }
+}
+
+/* Método que imprime los valores almacenados con forma de arreglo 
+bidimensional. */
+template <class T>
+void MatrizSimetrica<T>::ImprimeMatriz()
+{
+    int Ren, Col, Indice;
+    cout << "\n\nMatriz simetrica\n\n";
+    for (Ren= 0; Ren < Dim; Ren++)
+    {
+        for (Col= 0; Col < Dim; Col++) 
+        {
+            if (Col <= Ren)
+            {
+                Indice= RegresaPosic(Ren, Col);
+                cout << Datos[Indice] << "\t";
+            }
+            else {
+                Indice= RegresaPosic(Col, Ren);
+                cout << Datos[Indice] << "\t";
+            }
+        
+        }
+        cout << "\n";
+    }
+    cout << "\n\n";
+}
+
+/* Método que imprime sólo los valores almacenados. */
+template <class T>
+void MatrizSimetrica<T>::ImprimeDatos()
+{
+    int Indice, TotElem;
+    TotElem= TotalDatos();
+    cout << "\n\nElementos de la matriz simetrica\n\n";
+    for (Indice= 0; Indice < TotElem; Indice++)
+        cout << Datos[Indice] << " ";
+    cout << "\n\n";
+}
+
+void usaMatrizSimetrica()
+{
+    MatrizSimetrica<int> matriz = MatrizSimetrica<int>();
+    matriz.Lectura();
+    matriz.ImprimeDatos();
+    matriz.ImprimeMatriz();
+}
+
+int main()
+{
+    usaMatrizSimetrica();
+    return 0;
+}
