107034f6f470dadc5f138ef0143772553735735e
jibaru <xxnacho1997xx@gmail.com>
2020-08-30 14:40:44 -0500

    Seccion 5 ejercicio 8

--- 5.PILAS_COLAS/EJERCICIOS/8.cpp
+++ 5.PILAS_COLAS/EJERCICIOS/8.cpp
@@ -9,3 +9,65 @@ Puede usar cualquier estructura de datos como auxiliar.
          ^           ^
        Frente      Final
 */
+#include <iostream>
+#include "../EJEMPLOS/5.7.cpp"
+using namespace std;
+
+template <class T>
+void eliminaRepetidos(ColaCircular<T>& cola)
+{
+	ColaCircular<T> aux = ColaCircular<T>();
+	T dato = 0, anterior = 0;
+	
+	while(cola.ColaCircularVacia() != 1) {
+		cola.EliminaCCircular(&dato);
+		aux.InsertaCCircular(dato);
+	}
+	
+	while(aux.ColaCircularVacia() != 1) {
+		
+		aux.EliminaCCircular(&dato);
+		
+		if(dato != anterior) {
+			cola.InsertaCCircular(dato);
+		}
+		anterior = dato;
+	}
+}
+
+template <class T>
+void muestraColaCircular(ColaCircular<T> cola){
+	ColaCircular<T> aux = cola;
+	T dato = 0;
+	
+	while(cola.ColaCircularVacia() != 1) {
+		cola.EliminaCCircular(&dato);
+		cout << " <- " << dato;
+	}
+}
+
+void UsaColaCirculaEjercicio8()
+{
+	ColaCircular<int> cola = ColaCircular<int>();
+	
+	cola.InsertaCCircular(87);
+	cola.InsertaCCircular(54);
+	cola.InsertaCCircular(54);
+	cola.InsertaCCircular(54);
+	cola.InsertaCCircular(91);
+	cola.InsertaCCircular(91);
+	
+	cout << "\nCola Circular: ";
+	muestraColaCircular(cola);
+	
+	eliminaRepetidos(cola);
+	
+	cout << "\nCola Circular sin repetidos: ";
+	muestraColaCircular(cola);
+}
+
+int main()
+{
+	UsaColaCirculaEjercicio8();
+	return 0;
+}
