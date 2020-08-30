58f3088b201704c97655be67823462a6e95a2b2c
jibaru <xxnacho1997xx@gmail.com>
2020-08-30 11:53:10 -0500

    Seccion 5 ejercicio 4

--- 5.PILAS_COLAS/EJERCICIOS/4.cpp
+++ 5.PILAS_COLAS/EJERCICIOS/4.cpp
@@ -8,3 +8,47 @@ Tope -> [ 623]      [ 928] <- Tope
         [3209]      [1024]
         [ 928]      [ 623]
 */
+#include "../EJEMPLOS/5.1.cpp"
+
+template <class T>
+void inviertePila(Pila<T>& pila)
+{
+	T dato;
+	Pila<T> aux = pila;
+	
+	// vaciar la pila
+	while(pila.PilaVacia() != 1) {
+		pila.Pop(&dato);
+	}
+	
+	// agregar los elementos de aux en la pila
+	while(aux.PilaVacia() != 1) {
+		aux.Pop(&dato);
+		pila.Push(dato);
+	}
+}
+
+void UsaPilaEjercicio4() 
+{
+	Pila<int> pila = Pila<int>();
+	
+	pila.Push(928);
+	pila.Push(3209);
+	pila.Push(1024);
+	pila.Push(623);
+	
+	cout << "\nPila: " << endl;
+	pila.ImprimeDatos();
+	
+	inviertePila(pila);
+	
+	cout << "\nPila invertida: " << endl;
+	pila.ImprimeDatos();
+	
+}
+
+int main()
+{
+	UsaPilaEjercicio4();
+	return 0;
+}
