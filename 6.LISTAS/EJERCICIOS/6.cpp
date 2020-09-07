1c7e0b39297c551bcb3f06e308a9378fb6fa1eee
jibaru <xxnacho1997xx@gmail.com>
2020-09-06 22:33:51 -0500

    Seccion 6 ejercicio 6

--- 6.LISTAS/EJERCICIOS/6.cpp
+++ 6.LISTAS/EJERCICIOS/6.cpp
@@ -11,3 +11,143 @@ la lista debería quedar como lo muestra la parte b).
 a) Primero->3->5->8->9->NULL
 b) Primero->3->4->5->6->7->8->9->NULL
 */
+
+#include <iostream>
+
+class ListaSimLigada;
+
+class NodoLista
+{
+private:
+	int info;
+	NodoLista* siguiente;
+public:
+	NodoLista();
+	int regresaInfo();
+	friend class ListaSimLigada;
+};
+
+class ListaSimLigada 
+{
+private:
+	NodoLista* primero;
+	NodoLista* ultimo;
+public:
+	ListaSimLigada();
+	~ListaSimLigada();
+	void imprimir();
+	void insertaFinal(int);
+	int tamanio();
+	int vacia();
+	void rellenaFaltantesOrdenada();
+};
+
+NodoLista::NodoLista()
+{
+	siguiente = NULL;
+}
+
+int NodoLista::regresaInfo()
+{
+	return info;
+}
+
+ListaSimLigada::ListaSimLigada()
+{
+	primero = NULL;
+	ultimo = NULL;
+}
+
+ListaSimLigada::~ListaSimLigada()
+{
+	NodoLista* aux;
+	while(primero) {
+		aux = primero;
+		primero = primero->siguiente;
+		delete aux;
+	}
+}
+
+void ListaSimLigada::imprimir()
+{
+	NodoLista* aux = primero;
+	
+	while(aux) {
+		std::cout << aux->info << " -> ";
+		aux = aux->siguiente;
+	}
+}
+
+void ListaSimLigada::insertaFinal(int dato)
+{
+	NodoLista* nuevoNodo = new NodoLista();
+	if(nuevoNodo == NULL) {
+		std::cout << "SIN MEM" << std::endl;
+	}
+	nuevoNodo->info = dato;
+	if(primero == NULL && ultimo == NULL){
+		primero = nuevoNodo;
+		ultimo = nuevoNodo;
+	} else {
+		ultimo->siguiente = nuevoNodo;
+		ultimo = nuevoNodo;
+	}
+}
+
+int ListaSimLigada::tamanio()
+{
+	int cont = 0;
+	NodoLista* aux = primero;
+	if(primero) {
+		while(aux != NULL) {
+			cont++;
+			aux = aux->siguiente;
+		}
+	}
+
+	return cont;
+}
+
+int ListaSimLigada::vacia()
+{
+	if(primero == NULL && ultimo == NULL) {
+		return 1;
+	}
+	return 0;
+}
+
+void ListaSimLigada::rellenaFaltantesOrdenada()
+{
+	NodoLista* actual,*nuevo;
+	actual = primero;
+	while(actual) {
+		if(actual != ultimo) {
+			if(actual->info != actual->siguiente->info - 1) {
+				nuevo = new NodoLista();
+				nuevo->info = actual->info + 1;
+				nuevo->siguiente = actual->siguiente;
+				actual->siguiente = nuevo;
+			}
+		}
+		actual = actual->siguiente;
+	}
+}
+
+int main()
+{
+	ListaSimLigada lista = ListaSimLigada();
+	lista.insertaFinal(3);
+	lista.insertaFinal(5);
+	lista.insertaFinal(8);
+	lista.insertaFinal(9);
+	
+	std::cout << "\nLista: ";
+	lista.imprimir();
+	
+	lista.rellenaFaltantesOrdenada();
+	
+	std::cout << "\nLista completa: ";
+	lista.imprimir();
+	
+	return 0;
+}
