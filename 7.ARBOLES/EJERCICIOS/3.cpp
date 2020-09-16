678409568952dbd2bc784f92d5d7ee06b47194b9
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-16 11:10:26 -0500

    Seccion 7 ejercicio 3

--- 7.ARBOLES/EJERCICIOS/3.cpp
+++ 7.ARBOLES/EJERCICIOS/3.cpp
@@ -2,3 +2,26 @@
 3.	Escriba un método que cuente el número de nodos intermedios de un árbol 
 binario. ¿Podría resolverlo por medio de una función? Justifique su respuesta.
 */
+#include <iostream>
+#include "../../lib/arboles/ArbolBinBusqueda.h"
+
+using namespace std;
+
+int main()
+{
+    ArbolBinBus<int> arbol = ArbolBinBus<int>();
+
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 156);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 99);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 360);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 67);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 258);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 508);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 301);
+
+    arbol.Preorden(arbol.RegresaRaiz());
+
+    cout << "\nCantidad nodos intermedios: " << arbol.NumeroNodosIntermedios() << endl;
+    
+    return 0;
+}
