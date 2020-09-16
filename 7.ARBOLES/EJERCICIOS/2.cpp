180a7ed2cb4acd1a86bd3a98f15f451f6293c399
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-16 10:54:59 -0500

    Seccion 7 ejercicio 2

--- 7.ARBOLES/EJERCICIOS/2.cpp
+++ 7.ARBOLES/EJERCICIOS/2.cpp
@@ -2,3 +2,26 @@
 2.	Escriba un método que cuente el número de hojas de un árbol binario. 
 ¿Podría resolverlo por medio de una función? Justifique su respuesta.
 */
+#include <iostream>
+#include "../../lib/arboles/ArbolBinBusqueda.h"
+
+using namespace std;
+
+int main()
+{
+    ArbolBinBus<int> arbol = ArbolBinBus<int>();
+
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 156);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 99);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 360);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 67);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 258);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 508);
+    arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 301);
+
+    arbol.Preorden(arbol.RegresaRaiz());
+
+    cout << "\nCantidad Hojas: " << arbol.NumeroHojas() << endl;
+    
+    return 0;
+}
