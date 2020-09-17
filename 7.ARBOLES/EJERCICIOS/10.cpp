2416cf303aa9d947c9783a2278e82c991f4c4be3
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-17 11:01:39 -0500

    Seccion 7 ejercicio 10

--- 7.ARBOLES/EJERCICIOS/10.cpp
+++ 7.ARBOLES/EJERCICIOS/10.cpp
@@ -6,3 +6,24 @@ la impresión sería:
 
 X-Z-A-M-K-P
 */
+#include <iostream>
+#include "../../lib/arboles/ArbolBinBusqueda.h"
+
+using namespace std;
+
+int main()
+{
+  ArbolBinBus<char> arbol = ArbolBinBus<char>();
+
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'M');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'J');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'Q');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'I');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'K');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'O');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'S');
+
+  arbol.ImprimePorNiveles();
+
+  return 0;
+}
