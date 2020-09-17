b655b4efbf753ea7e8474b3e114360e54872ae70
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-17 10:25:20 -0500

    Seccion 7 ejercicio 8

--- 7.ARBOLES/EJERCICIOS/8.cpp
+++ 7.ARBOLES/EJERCICIOS/8.cpp
@@ -10,3 +10,31 @@ equivalentes. En la siguiente figura se presenta un ejemplo de dos
       M   K   P           M   K   P
 
 */
+#include <iostream>
+#include "1.cpp"
+
+using namespace std;
+
+int main()
+{
+  ArbolMultiple<char> arbol1 = ArbolMultiple<char>();
+  ArbolMultiple<char> arbol2 = ArbolMultiple<char>();
+  
+  arbol1.insertarRaiz('X');
+  arbol1.insertar('Z', 'X');
+  arbol1.insertar('A', 'X');
+  arbol1.insertar('M', 'Z');
+  arbol1.insertar('K', 'A');
+  arbol1.insertar('P', 'A');
+
+  arbol2.insertarRaiz('X');
+  arbol2.insertar('Z', 'X');
+  arbol2.insertar('A', 'X');
+  arbol2.insertar('M', 'Z');
+  arbol2.insertar('K', 'A');
+  arbol2.insertar('P', 'A');
+
+  cout << "Arboles iguales: " << (arbol1.igual(arbol2) ? "IGUAL" : "DESIGUAL") << endl;
+
+  return 0;
+}
