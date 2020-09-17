853df8c046fdade8fac7e3dac1825f93e9e9e1f1
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-17 10:36:38 -0500

    Seccion 7 ejercicio 9

--- 7.ARBOLES/EJERCICIOS/9.cpp
+++ 7.ARBOLES/EJERCICIOS/9.cpp
@@ -5,7 +5,28 @@ que determine si un árbol (dado como parámetro) es completo.
 En la siguiente figura se presenta un ejemplo de un árbol 
 que tiene esta característica.
 
-        T
-    J       V
-  M  Q     G  S
+        M
+    J       Q
+  I  K     O  S
 */
+#include <iostream>
+#include "../../lib/arboles/ArbolBinBusqueda.h"
+
+using namespace std;
+
+int main()
+{
+  ArbolBinBus<char> arbol = ArbolBinBus<char>();
+
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'M');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'J');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'Q');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'I');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'K');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'O');
+  arbol.InsertaNodoSinRep(arbol.RegresaRaiz(), 'S');
+
+  cout << "Arbol Completo: " << (arbol.EsCompleto() ? "COMPLETO" : "INCOMPLETO") << endl;
+
+  return 0;
+}
