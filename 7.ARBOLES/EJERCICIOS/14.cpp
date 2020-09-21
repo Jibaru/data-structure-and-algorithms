ce67dc7588e83393169447ef5f235dd79ba3cd00
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-21 10:59:57 -0500

    Seccion 7 ejercicio 14

--- 7.ARBOLES/EJERCICIOS/14.cpp
+++
@@ -1,8 +0,0 @@
-/*
-14.	Inserte los siguientes datos en un áibol-B, de grado 2. 
-Los números que se dan como datos pueden representar datos más 
-complejos (objetos). Dibuje el árbol a medida que sufra cambios 
-en su estructura como consecuencia de la inserción.
-Insertar: 
-95 - 10 - 34 - 87 - 56 - 99 - 12 - 23 - 50 - 40 - 60 - 54 - 33 - 20-91- 17-18-94
-*/
