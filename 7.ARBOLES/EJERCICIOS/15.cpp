07081e62d294ad864ef20802e763708da5acd7e4
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-21 11:45:54 -0500

    Seccion 7 ejercicio 15

--- 7.ARBOLES/EJERCICIOS/15.cpp
+++
@@ -1,6 +0,0 @@
-/*
-15.	En el árbol-B generado en el problema anterior elimine los 
-datos que se señalan a continuación. Dibuje el árbol a medida 
-que sufra cambios en su estructura como consecuencia de la eliminación.
-Eliminar: 99 - 60 - 23 - 12 - 95 - 40
-*/
