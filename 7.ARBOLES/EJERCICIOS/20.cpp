507d7a8b406c57a0052c559d2775ca4e659903a7
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-21 20:11:52 -0500

    Seccion 7 ejercicios 18 y 20

--- 7.ARBOLES/EJERCICIOS/20.cpp
+++
@@ -1,9 +0,0 @@
-/*
-20.	Inserte los siguientes datos en un árbol-B+, de grado 2. 
-Los números que se dan como datos pueden representar a datos 
-más complejos (objetos). Dibuje el árbol a medida que sufra 
-cambios en su estructura como consecuencia de la inserción.
-Insertar: 
-350 - 180 - 420 - 700 - 390 - 200 - 150 - 400 - 300 - 100 - 
-500 - 310 - 660 - 580 - 880 - 670 - 370 - 140 - 230 - 490 - 510
-*/
