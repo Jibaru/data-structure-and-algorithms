d5351c1140a69c0a9266c38af67d77f33582f492
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-30 10:54:46 -0500

    Seccion 8 ejercicio 6

--- 8.GRAFICAS/EJERCICIOS/6.cpp
+++ 8.GRAFICAS/EJERCICIOS/6.cpp
@@ -3,3 +3,20 @@
 conveniente para encontrar e imprimir el árbol abarcador de costo mínimo 
 correspondiente a dicha gráfica.
 */
+
+#include <iostream>
+#include "../../lib/graficas/Grafica.h"
+
+int main() 
+{
+
+    Grafica<int> grafica;
+
+    grafica.Lee();
+    
+    // Método para encontrar e imprimir el árbol 
+    //abarcador de costo mínimo
+    grafica.Kruskal();
+
+    return 0;
+}
