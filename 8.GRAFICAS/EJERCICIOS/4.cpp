040e88e1fa413868cf351febee2d571f17891179
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-30 10:38:12 -0500

    Seccion 8 ejercicio 4

--- 8.GRAFICAS/EJERCICIOS/4.cpp
+++ 8.GRAFICAS/EJERCICIOS/4.cpp
@@ -3,3 +3,20 @@
 una matriz que indique si existe o no un camino entre cada uno de los vértices de la 
 gráfica dirigida.
 */
+#include <iostream>
+#include "../../lib/graficas/Digrafica.h"
+
+int main() 
+{
+
+    DiGrafica<int> digrafica;
+
+    digrafica.Lee();
+    
+    // Método que generar una matriz que indica si existe o no 
+    // un camino entre cada uno de los vértices de la gráfica dirigida
+    digrafica.Warshall();
+    digrafica.Imprime(1);
+
+    return 0;
+}
