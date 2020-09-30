c47477cca1bdaefc8b55977dec7d414ccc6dd439
Ignacio <ignacioruedaboada@hotmail.com>
2020-09-30 10:34:40 -0500

    Seccion 8 ejercicio 3

--- 8.GRAFICAS/EJERCICIOS/3.cpp
+++ 8.GRAFICAS/EJERCICIOS/3.cpp
@@ -3,3 +3,20 @@
 obtener e imprimir los caminos de mínimos costos entre el vértice 3 y los 
 demás vértices de la digráfica.
 */
+#include <iostream>
+#include "../../lib/graficas/Digrafica.h"
+
+int main() 
+{
+
+    DiGrafica<int> digrafica;
+
+    digrafica.Lee();
+    
+    // Método que imprime los caminos de costos mínimos entre
+    // el vértice 3 y los demás vértices (Dijsktra)
+    digrafica.Dijkstra();
+    digrafica.Imprime(4);
+
+    return 0;
+}
